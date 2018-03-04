06659aab36b2a77379137736e0f0a794578777b7
lex <lex@users.noreply.github.com>
2018-03-04 22:39:12 +0200

    6.1

--- redux-anecdotes-v2/src/components/AnecdoteList.js
+++ redux-anecdotes-v2/src/components/AnecdoteList.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 class AnecdoteList extends React.Component {
-  render() {
-    const anecdotes = this.props.store.getState()
-    return (
-      <div>
-        <h2>Anecdotes</h2>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-          <div key={anecdote.id}>
+    render() {
+        const anecdotes = this.props.store.getState();
+
+        return (
             <div>
-              {anecdote.content}
+                <h2>Anecdotes</h2>
+                {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote => (
+                    <div key={anecdote.id}>
+                        <div>{anecdote.content}</div>
+
+                        <div>
+                            has {anecdote.votes}
+                            <button
+                                onClick={() =>
+                                    this.props.store.dispatch({
+                                        type: 'VOTE',
+                                        id: anecdote.id,
+                                    })
+                                }
+                            >
+                                vote
+                            </button>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => 
-                this.props.store.dispatch({ type: 'VOTE', id: anecdote.id })
-              }>
-                vote
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-    )
-  }
+        );
+    }
 }
 
-export default AnecdoteList
+export default AnecdoteList;
